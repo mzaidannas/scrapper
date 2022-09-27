502eb774a1fda8aa94619662b2d2be1addfdd09d
Zaid Annas <m.zaid.annas@gmail.com>
2022-09-27 13:11:59 +0500

    Avo license override

--- app/overrides/avo_license_override.rb
+++ app/overrides/avo_license_override.rb
@@ -5,3 +5,19 @@ Avo::ApplicationController.class_eval do
 
   def check_avo_license; end
 end
+
+Avo::Licensing::HQ.class_eval do
+  def response
+    expire_cache_if_overdue
+
+    # ------------------------------------------------------------------
+    # You could set this to true to become a pro user for free.
+    # I'd rather you didn't. Avo takes time & love to build,
+    # and I can't do that if it doesn't pay my bills!
+    #
+    # If you want Pro, help pay for its development.
+    # Can't afford it? Get in touch: adrian@avohq.io
+    # ------------------------------------------------------------------
+    true
+  end
+end
