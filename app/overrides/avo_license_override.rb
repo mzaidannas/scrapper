ce52ec74d4c701587433dd0ff902b68c1ab3a420
Zaid Annas <m.zaid.annas@gmail.com>
2022-09-28 14:14:36 +0500

    Fixed pro lisence

--- app/overrides/avo_license_override.rb
+++ app/overrides/avo_license_override.rb
@@ -8,16 +8,6 @@ end
 
 Avo::Licensing::HQ.class_eval do
   def response
-    expire_cache_if_overdue
-
-    # ------------------------------------------------------------------
-    # You could set this to true to become a pro user for free.
-    # I'd rather you didn't. Avo takes time & love to build,
-    # and I can't do that if it doesn't pay my bills!
-    #
-    # If you want Pro, help pay for its development.
-    # Can't afford it? Get in touch: adrian@avohq.io
-    # ------------------------------------------------------------------
-    true
+    {"id": "pro", "valid": true, "payload": {}}
   end
 end
