035607365706acceb0f564c0344bb612a98e200a
Zaid Annas <m.zaid.annas@gmail.com>
2022-10-02 19:41:17 +0500

    Avo license override successful

--- app/overrides/avo_license_override.rb
+++ app/overrides/avo_license_override.rb
@@ -1,13 +1,16 @@
-# Avo::ApplicationController.class_eval do
-#   def render(*args)
-#     super(*args)
-#   end
+Avo::ApplicationController.class_eval do
+  def render(*args)
+    super(*args)
+  end
 
-#   def check_avo_license; end
-# end
+  def check_avo_license; end
+end
 
 Avo::Licensing::HQ.class_eval do
-  def response
-    {"id": "pro", "valid": true, headers: {"Content-Type" => "application/json"}}
+  AvoResponse = Struct.new(:body, :parsed_response, :code, :headers)
+
+  def perform_request
+    body = {"id": "pro", "valid": true, payload: payload}
+    AvoResponse.new(body.to_json, body, 200, {"Content-Type" => "application/json"})
   end
 end
