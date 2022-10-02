4cd4c556df36522c7400f88eda7430a57fa8cf03
Zaid Annas <m.zaid.annas@gmail.com>
2022-10-02 19:04:47 +0500

    Avo License override experiment 2

--- app/overrides/avo_license_override.rb
+++ app/overrides/avo_license_override.rb
@@ -1,13 +1,13 @@
-Avo::ApplicationController.class_eval do
-  def render(*args)
-    super(*args)
-  end
+# Avo::ApplicationController.class_eval do
+#   def render(*args)
+#     super(*args)
+#   end
 
-  def check_avo_license; end
-end
+#   def check_avo_license; end
+# end
 
 Avo::Licensing::HQ.class_eval do
   def response
-    {"id": "pro", "valid": true, "payload": {}}
+    {"id": "pro", "valid": true, headers: {"Content-Type" => "application/json"}}
   end
 end
