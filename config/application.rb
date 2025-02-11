9b83346c9c4258c5a37c8ba3d5ba89fb4fa3570c
Zaid Annas <m.zaid.annas@gmail.com>
2025-02-11 15:22:24 +0500

    Fixed override file loading

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,7 @@ module Scrapper
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
+    Rails.autoloaders.main.ignore(Rails.root.join('app/overrides'))
 
     # Configuration for the application, engines, and railties goes here.
     #
